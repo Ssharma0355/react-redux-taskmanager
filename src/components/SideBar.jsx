25f8c4e22584042a5d7c4d93631a1660cd35dd6e
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-12-28 19:56:27 +0530

    sidebar-progress-1

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div>
-      Sidebar
+    <div className='lg:col-span-1 space-y-6'>
+        <div className='bg-gray-800 rounded-xl p-6 text-white'>
+            <h1 className='text-sm opacity-50'>Feedback Board</h1>
+        </div>
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SideBar from '../components/SideBar'
+import { Camera } from 'lucide-react';
 
 const HomePage = () => {
   return (
@@ -7,6 +8,7 @@ const HomePage = () => {
         <div className='grid grid-cols-1 lg:grid-cols-4 gap-6'>
             <SideBar />
             Sachin maoin
+            <Camera color="red" size={48} />
         </div>
     </div>
   )
