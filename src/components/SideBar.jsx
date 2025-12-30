48ce6e789e5e40839cb2d62f08ad2bc60eaab41c
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-12-30 14:06:49 +0530

    side-bar-progress

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,22 +2,66 @@ import React from 'react'
 
 
 const SideBar = () => {
-  return (
-    <div className='lg:col-span-1 space-y-6'>
-        <div className='bg-gray-800 rounded-xl p-6 text-white'>
-            <h1 className='text-xl font-bold mb-1'>Client</h1>
-            <p className='text-sm opacity-50'>Feedback Board</p>
+    const Categories =[
+        {
+        name:"Create Task"
+    },
+
+]
+    return (
+        <div className='lg:col-span-1 space-y-6'>
+            <div className='bg-gray-800 rounded-xl p-6 text-white'>
+                <h1 className='text-xl font-bold mb-1'>Client</h1>
+                <p className='text-sm opacity-50'>Feedback Board</p>
             </div>
             <div className='bg-white rounded-xl p-8 shadow-md'>
                 <div className='flex flex-wrap gap-2'>
                     <button className={`p-4 py-2 text-black rounded-lg text-sm font-semibold transistion-all`}>
-                    Categories
+                        Categories
                     </button>
                 </div>
-
             </div>
-    </div>
-  )
+            <div className='bg-white rounded-xl p-6 shadow-md'>
+                <div className='flex justify-between items-center mb-4'>
+                    <h2 className='font-bold text-gray-800'>RoadMap</h2>
+                    <button className='text-blue-600 text-sm font-semibold'> View </button>
+                </div>
+                <div className="space-y-3">
+                    <div className="flex justify-between items-center">
+                        <div className="flex items-center gap-2"> 
+                        <div className="w-2 h-2 rounded-full bg-orange-400">
+                        </div>
+                        <span className="text-gray-600 text-sm">Planned</span>
+                    </div>
+                    <span className="font-bold text-gray-600" >Panned</span></div>
+                </div>
+                <div className="space-y-3">
+                    <div className="flex justify-between items-center">
+                        <div className="flex items-center gap-2"> 
+                        <div className="w-2 h-2 rounded-full bg-purple-400">
+                        </div>
+                        <span className="text-gray-600 text-sm">In Progress</span>
+                    </div>
+                    <span className="font-bold text-gray-600" >In Progress</span></div>
+                </div>
+                <div className="space-y-3">
+                    <div className="flex justify-between items-center">
+                        <div className="flex items-center gap-2"> 
+                        <div className="w-2 h-2 rounded-full bg-green-400">
+                        </div>
+                        <span className="text-gray-600 text-sm">Live</span>
+                    </div>
+                    <span className="font-bold text-gray-600" >Live</span></div>
+                </div>
+                <div className='mt-4'>
+                    <button className='w-full bg-purple-700 hover:bg-purple-800 px-4 py-2 rounded-lg font-semibold text-sm trasnition-all text-white'>
+                        + Add Feedback
+                    </button>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default SideBar
