51167c602e8e893ae073a9cf037d217347994a85
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-12-30 14:13:25 +0530

    ages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,16 @@ import SideBar from '../components/SideBar'
 
 const HomePage = () => {
   return (
-    <div className='max-w-6xl mx-auto'>
+    <div className='max-w-6xl mx-auto p-4'>
         <div className='grid grid-cols-1 lg:grid-cols-4 gap-6'>
             <SideBar />
-            Main Content
+            <div className='lg:col-span-3 space-y-6'>
+              <div className='bg-gray-800 rounded-lg p-4 flex flex-col sm:flex-row items-start sm:item-center justify-between gap-4'>
+
+              
+              </div>
+
+            </div>
         </div>
     </div>
   )
