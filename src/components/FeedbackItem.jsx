b69d4d44c00b2eed3d1447368e230f643d16e388
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-12-30 14:31:12 +0530

    need to show records

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FeedbackItem = () => {
+  return (
+    <div>
+        Feed back Items
+      
+    </div>
+  )
+}
+
+export default FeedbackItem
---
+++ src/components/NoRecord.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NoRecord = () => {
+  return (
+    <div className='flex flex-center items-center'>
+        <h1 className='font-bold text-lg text-gray-800'>Sorry!! No Records found!</h1>
+      
+    </div>
+  )
+}
+
+export default NoRecord
