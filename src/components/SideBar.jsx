3535edb6b148040953ea042c43c00ea73e30684d
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2025-12-30 12:30:08 +0530

    categories map

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import { Icon } from 'lucide-react';
-import { coconut } from '@lucide/lab';
 
 
 const SideBar = () => {
   return (
     <div className='lg:col-span-1 space-y-6'>
         <div className='bg-gray-800 rounded-xl p-6 text-white'>
-            <h1 className='text-sm opacity-50'>Feedback Board</h1>
-            <Icon iconNode={coconut} />
+            <h1 className='text-xl font-bold mb-1'>Client</h1>
+            <p className='text-sm opacity-50'>Feedback Board</p>
+            </div>
+            <div className='bg-white rounded-xl p-8 shadow-md'>
+                <div className='flex flex-wrap gap-2'>
+                    <button className={`p-4 py-2 text-black rounded-lg text-sm font-semibold transistion-all`}>
+                    Categories
+                    </button>
+                </div>
 
-        </div>
+            </div>
     </div>
   )
 }
