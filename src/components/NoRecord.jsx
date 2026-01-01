8f19e021f2d8687d7692494d860c5c6fe263aa52
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-01 14:31:56 +0530

    saved

--- src/components/NoRecord.jsx
+++ src/components/NoRecord.jsx
@@ -4,6 +4,7 @@ const NoRecord = () => {
   return (
     <div className='flex flex-center items-center text-center'>
         <h1 className='font-bold text-lg text-gray-800'>Sorry!!SSS No Records found!</h1>
+        <h1 className='font-bold text-lg text-gray-800'>Sorry!!SSS No Records found!</h1>
       
     </div>
   )
