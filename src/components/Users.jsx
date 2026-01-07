a6b0793c9b1abfcf34023a0298e3582e4d073718
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-07 10:54:44 +0530

    added to user api

---
+++ src/components/Users.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+
+const Users = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [name, setName] = useState("");
+    useEffect(()=>{
+        const fetchUsers = async ()=>{
+            try{
+                const resposne = await axios
+                .get("https://jsonplaceholder.typicode.com/users");
+                setUsers(resposne.data)
+            }
+            catch(err){
+                if(err.resposne){
+                    console.log("Error in response")
+                }
+
+            }
+            finally{
+                setLoading(false)
+        }
+        }
+        fetchUsers();
+    
+    },[])
+
+   
+    const postName = async () => {
+        try {
+          const response = await axios.post(
+            "https://jsonplaceholder.typicode.com/users",
+            { name }
+          );
+    
+          // Optimistic UI update
+          setUsers([...users, response.data]);
+          setName("");
+        } catch (error) {
+          alert("Failed to add user");
+        }
+      };
+
+    if(loading){
+        <h1>Loadingg...</h1>
+    }
+  return (
+    <div>
+        <div>
+        <p>Enter user</p>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="here"
+        />
+        <button onClick={postName}>Submit name</button>
+      </div>
+        {users.map((user)=>(
+            <li key={user.id}>{user.name}</li>
+        ))}
+
+      
+    </div>
+  )
+}
+
+export default Users
